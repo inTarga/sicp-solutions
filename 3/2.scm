06f6fdde25d9d003a3913c879f266bdfff469ac7
Ingrid Rebecca Abraham <ingridra@met.no>
2022-03-28 13:37:11 +0200

    3.2 solutions

---
+++ 3/2.scm
@@ -0,0 +1,14 @@
+; 3.9
+
+; not going to try and reproduce the diagrams in ascii for this or the two below
+
+; 3.10
+
+; the lambda results in an extra redundant frame
+
+; 3.11
+
+; The local state for acc is kept in its own frame, distinction is enforced
+; by the creation of a separate frame for each invocation of acc.
+
+; They share everything in the global env
